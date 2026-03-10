b61dc9eaa1d9459290d0cb865c2182f97ff62c07
khushbooydv1307-srm <khushbooydv1307@gmail.com>
2026-03-10 09:36:24 +0530

    updated HelloApp.java

--- HelloApp.java
+++ HelloApp.java
@@ -1,14 +1,7 @@
 public class HelloApp {
+
     public static void main(String[] args) {
-
-        // Check if a name argument is provided
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            // Default message if no argument is provided
-            System.out.println("Hello, World!");
-        }
-
+        System.out.println("Hello World");
     }
+
 }
